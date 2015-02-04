f3c75ba27249cce664ac9beeeb7aaac1801be812
nae.ikari <zhukrmail@gmail.com>
2015-02-05 00:15:59 +0800

    ComLineTool-commands-support
    commands support for command_line_tool including ls mkdir cd & rm. Add
    keyboard bingding.

--- src/ASA/cms/static/js/web_terminal.js
+++ src/ASA/cms/static/js/web_terminal.js
@@ -2,25 +2,29 @@ var app = angular.module("web_terminal", []).config(function($interpolateProvide
     $interpolateProvider.startSymbol('{$');
     $interpolateProvider.endSymbol('$}');
 });
-		app.controller("f", function($scope, $http){
-			$scope.user = "ASA.tv";
-			$scope.host = "psycium.xyz";
-			$scope.pwd = "\\";
-			$scope.coms = [];
-			$scope.inp = "";
-			
-			//$http.get("../cms/.");
-
-			$scope.submit = function(){
-				$http.get("../cms/" + encodeURI($scope.inp)).
-					success(function(data, status, headers, config){
-						$scope.oup = status == 200 ? data.msg : "encountered an error!";
-					}).
-					error(function(data, status, headers, config){
-						$scope.oup = "encontered an error!";
-					});
-				//$scope.oup = "msg has been sent";
+app.controller("f", ["$scope", "$http", function($scope, $http){
+	$scope.user = "ASA.tv";
+	$scope.host = "psycium.xyz";
+	$scope.pwd = "/";
+	$scope.coms = [];
+	$scope.inp = "";
+	
+	$scope.submit = function(){
+		$http.get("../cms/" + encodeURI($scope.inp)).
+			success(function(data, status, headers, config){
+				$scope.oup = status == 200 ? data.msg : "encountered an error!";
 				$scope.coms.push({inp:$scope.inp, oup:$scope.oup});
 				$scope.inp = "";
-			}
-		});
+			}).
+			error(function(data, status, headers, config){
+				$scope.oup = [["encontered an error!"]];
+				$scope.coms.push({inp:$scope.inp, oup:$scope.oup});
+				$scope.inp = "";
+			});
+	}
+	
+	$scope.detectEnter = function(event){
+		if(event.keyIdentifier == "Enter")
+			$scope.submit();
+	}
+}]);
