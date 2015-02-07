c0a85682a45dd4d794af7c5d096c09af86e7294a
nae.ikari <zhukrmail@gmail.com>
2015-02-07 21:07:13 +0800

    fix bugs
    now it can run!

--- src/ASA/cms/static/js/web_terminal.js
+++ src/ASA/cms/static/js/web_terminal.js
@@ -2,6 +2,7 @@ var app = angular.module("web_terminal", []).config(function($interpolateProvide
     $interpolateProvider.startSymbol('{$');
     $interpolateProvider.endSymbol('$}');
 });
+
 app.controller("f", ["$scope", "$http", function($scope, $http){
 	$scope.user = "ASA.tv";
 	$scope.host = "psycium.xyz";
@@ -10,21 +11,39 @@ app.controller("f", ["$scope", "$http", function($scope, $http){
 	$scope.inp = "";
 	
 	$scope.submit = function(){
-		$http.get("../cms/" + encodeURI($scope.inp)).
-			success(function(data, status, headers, config){
-				$scope.oup = status == 200 ? data.msg : "encountered an error!";
-				$scope.coms.push({inp:$scope.inp, oup:$scope.oup});
-				$scope.inp = "";
+		var errorHandler = function(){
+			console.log("error");
+			$scope.com_type = "error";
+			$scope.coms.push({inp:$scope.inp, oup:{"status":"error","msg":"","com_type":$scope.com_type}});
+			$scope.inp = "";
+		}
+		
+		$http.get("/cms/" + encodeURI($scope.inp)).
+			success(function(data, status){
+				//console.log("success");
+				if(status != 200){
+					errorHandler();
+					return;
+				}
+				else{
+					$scope.com_type = $scope.inp.split(" ",1)[0];
+					$scope.oup = data;
+					$scope.coms.push({inp:$scope.inp, oup:$scope.oup,com_type:$scope.com_type});
+					$scope.inp = "";
+				}
 			}).
-			error(function(data, status, headers, config){
-				$scope.oup = [["encontered an error!"]];
-				$scope.coms.push({inp:$scope.inp, oup:$scope.oup});
-				$scope.inp = "";
-			});
+			error(errorHandler);
 	}
 	
 	$scope.detectEnter = function(event){
-		if(event.keyIdentifier == "Enter")
+		if(event.keyCode == 13)
 			$scope.submit();
+	}
+}]);
+
+app.controller("ls",["$scope",function($scope){
+	//console.log($scope.com);
+	if($scope.com.oup.status == "OK"){
+		$scope.com.oup.head = $scope.com.oup.msg.splice(0,1)[0];
 	}
 }]);
