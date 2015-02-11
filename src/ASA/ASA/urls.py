9e376f6bae86b14934c819898132962e2e69bd46
voidrank <voidrank@gmail.com>
2015-02-11 20:18:14 +0800

    fix-indent&urls

--- src/ASA/ASA/urls.py
+++ src/ASA/ASA/urls.py
@@ -3,12 +3,11 @@ from django.contrib import admin
 
 import video_cms
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'ASA.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include(video_cms)),
 )
-
--- src/ASA/video_cms/__init__.py
+++ src/ASA/video_cms/__init__.py
@@ -1,30 +1,63 @@
 from django.conf.urls import patterns, url, include
-from .upload_views import InitView, ChunkView, FinalizeView, DestroyView, PageView
+from .upload_views import InitView, ChunkView, FinalizeView, \
+    DestroyView, PageView
 from .player_views import MediaView, DownloadView, DanmakuView
 from django.views.decorators.csrf import csrf_exempt
 
 __all__ = ['exceptions', 'models', 'views']
 
-urlpatterns_upload = patterns('upload/',
-        url('init/?', csrf_exempt(InitView.as_view()), name='init'),
-        url('chunk/(?P<owner>[a-fA-F0-9]{64})/?', csrf_exempt(ChunkView.as_view()), name='chunk'),
-        url('store/(?P<owner>[a-fA-F0-9]{64})/?', csrf_exempt(FinalizeView.as_view()), name='store'),
-        url('destroy/(?P<owner>[a-fA-F0-9]{64}/?)', csrf_exempt(DestroyView.as_view()), name='destroy'),
-        url('', PageView.as_view(), name="upload")
+urlpatterns_upload = patterns(
+    r'',
+
+    url(
+        r'^upload/init/?',
+        csrf_exempt(InitView.as_view()),
+        name='init'
+    ),
+
+    url(
+        r'^upload/chunk/(?P<owner>[a-fA-F0-9]{64})/?',
+        csrf_exempt(ChunkView.as_view()),
+        name='chunk'
+    ),
+
+    url(
+        r'^upload/store/(?P<owner>[a-fA-F0-9]{64})/?',
+        csrf_exempt(FinalizeView.as_view()),
+        name='store'
+    ),
+
+    url(
+        r'^upload/destroy/(?P<owner>[a-fA-F0-9]{64}/?)',
+        csrf_exempt(DestroyView.as_view()),
+        name='destroy'
+    ),
+
+    url(
+        r'^upload/$',
+        PageView.as_view(),
+        name="upload"
+    )
 )
 
-urlpatterns_download = patterns('',
-        url(r'rec/(?P<filename>[a-zA-Z0-9_]{1,64})/?', MediaView.as_view()),
-        url(r'download/(?P<token>[a-zA-Z0-9]{64})/?', DownloadView.as_view())
+urlpatterns_download = patterns(
+    '',
+    url(r'^rec/(?P<filename>[a-zA-Z0-9_]{1,64})/?$', MediaView.as_view()),
+    url(r'^download/(?P<token>[a-zA-Z0-9]{64})/?$', DownloadView.as_view())
 )
 
-urlpatterns_danmaku = patterns('',
-        url(r'danmaku/(?P<token>[a-zA-Z0-9]{64})/?', csrf_exempt(DanmakuView.as_view())),
+urlpatterns_danmaku = patterns(
+    '',
+    url(
+        r'danmaku/(?P<token>[a-zA-Z0-9]{64})/?',
+        csrf_exempt(DanmakuView.as_view())
+    ),
 )
 
 
-urlpatterns = patterns('',
-        url('', include(urlpatterns_upload)),
-        url('', include(urlpatterns_download)),
-        url('', include(urlpatterns_danmaku)),
+urlpatterns = patterns(
+    '',
+    url('', include(urlpatterns_upload)),
+    url('', include(urlpatterns_download)),
+    url('', include(urlpatterns_danmaku)),
 )
