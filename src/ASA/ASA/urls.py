5bcd39b18c0c22183f6a00592da465a9d1ca1393
voidrank <voidrank@gmail.com>
2015-02-11 20:31:21 +0800

    fix-bug

--- src/ASA/ASA/urls.py
+++ src/ASA/ASA/urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('cms.urls', namespace='cms')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'admin/login.html'})
+        {'template_name': 'admin/login.html'}),
     url(r'', include(video_cms)),
 )
