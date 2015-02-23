4b569efa20d0800322e1465ce599e29314b82c89
voidrank <voidrank@gmail.com>
2015-02-23 18:57:29 +0800

    move danmaku & comment to website app

--- src/website/views.py
+++ src/website/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponseBadRequest, \
-    HttpResponse
+from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
@@ -111,7 +110,7 @@ class FinalizeView(video_cms.upload_views.FinalizeView):
                 **kwargs
             )
             data = json.loads(response.content)
-            data['path'] = '/home/'+str(request.user.username)+'/'+filename
+            data['path'] = '/home/' + str(request.user.username) + '/' + filename
             if ('errstr' in data) is False:
                 av(environ, [data['path'], str(data['rec'])])
             return response
@@ -170,3 +169,38 @@ class SessionsView(View):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(SessionsView, self).dispatch(*args, **kwargs)
+
+
+class DanmakuView(View):
+    @staticmethod
+    def get(request, token):
+        danmaku_list = Danmaku.load_danmaku_by_video_token(token)
+        return HttpResponse(json.dumps(danmaku_list))
+
+    @staticmethod
+    def post(request, token):
+        try:
+            data = request.POST
+            assert isinstance(data, dict)
+        except (ValueError, AssertionError):
+            return HttpResponseBadRequest(
+                {'errstr': 'invalid json format'},
+                content_type='application/json'
+            )
+
+        assert 'mode' in data
+        assert 'stime' in data
+        assert 'text' in data
+        assert 'color' in data
+        assert 'size' in data
+        assert 'date' in data
+        Danmaku.new(
+            owner=token,
+            date=int(data['date']),
+            mode=int(data['mode']),
+            stime=int(data['stime']),
+            text=data['text'],
+            color=data['color'],
+            size=int(data['size'])
+        )
+        return HttpResponse(json.dumps({'status': 'OK'}))
