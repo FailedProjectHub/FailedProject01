6ec909d302811d667969cb6fb35ec036a7d9570e
voidrank <voidrank@gmail.com>
2015-03-05 23:48:36 +0800

    daily commit

--- src/website/static/index/js/index.js
+++ src/website/static/index/js/index.js
@@ -13,25 +13,43 @@ var index = angular.module('index', []).config(
 index.controller('index', function index($scope, $http){
   /* element */
   $scope.collections = []; 
-  $http.post(
-    '/cms/', 
-    {
-      'command': ['ls', '/public', '--include=folderattrib'],
-      'path': '/'
-    }
-  ).success(
-    function(response){
-      for (var i in response.msg[0]){
-        $scope.collections.push({'name': response.msg[0][i].substr(8)});
+  
+  var chain = new Promise(function(){
+    $http.post(
+      '/cms/', 
+      {
+        'command': ['ls', '/public', '--include=folderattrib'],
+        'path': '/'
       }
-      console.log($scope.collections);
+    ).success(
+      function(response){
+        for (var i in response.msg[0]){
+          $scope.collections.push({'name': response.msg[0][i].substr(8)});
+        }
+        console.log($scope.collections);
+      }
+    ).error(
+      function(response){
+        console.log(response);
+      }
+    );
+  })
+  .then(function(){
+    for (var i in $scope.collections){
+      $http.post(
+        '/cms/',
+        {
+          'command': [
+            'ils', 
+            '--display=videofileattrib__video_file__filename', 
+            '--display=',
+            $scope.collections[i].name
+          ],
+          'path': '/'
+        }
+      )
     }
-  ).error(
-    function(response){
-      console.log(response);
-    }
-  );
-
+  })
   /* css */
   $scope.style = {};
   $scope.style.collections = {
