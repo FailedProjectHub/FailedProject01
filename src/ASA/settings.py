9329683d57149d782198356d1fd329521141c691
voidrank <voidrank@gmail.com>
2015-03-15 11:52:28 +0800

    fix bugs

--- src/ASA/settings.py
+++ src/ASA/settings.py
@@ -30,7 +30,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
+    "django.core.context_processors.debug",
+    "django.core.context_processors.static",
+    "django.core.context_processors.media",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.request"
 )
 
@@ -91,7 +94,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
 LOGGING_FILE = "log.log"
 LOGGING = {
     'version': 1,
