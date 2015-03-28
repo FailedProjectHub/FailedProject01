38a283d6bcc9fdc53b09be3fcb2e7b260cdc1791
voidrank <voidrank@gmail.com>
2015-03-28 23:39:34 +0800

    rm unused static files

--- src/ASA/settings.py
+++ src/ASA/settings.py
@@ -94,6 +94,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 LOGGING_FILE = "log.log"
 LOGGING = {
     'version': 1,
